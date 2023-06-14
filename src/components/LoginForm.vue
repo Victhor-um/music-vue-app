073517e041732b4cb09788290388f789c0d321b8
Victor <uberman59@gmail.com>
2023-06-14 15:01:14 +0500

    feat: decomposition

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="loginShowAlert"
+    :class="loginAlertVariant"
+  >
+    {{ loginAlertMsg }}
+  </div>
+  <vee-form :validation-schema="loginSchema" @submit="login">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        name="email"
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field
+        name="password"
+        type="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password"
+      />
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      :disabled="loginInSubmission"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const loginInSubmission = ref(false)
+const loginShowAlert = ref(false)
+const loginAlertVariant = ref('bg-blue-500')
+const loginAlertMsg = ref('Please wait! We are loggin you in.')
+
+import { ErrorMessage } from 'vee-validate'
+
+const loginSchema = {
+  email: 'required|min:3|max:100|email',
+  password: 'required|min:6|max:100|'
+}
+
+async function login(values) {
+  loginInSubmission.value = true
+  loginShowAlert.value = true
+
+  console.log(values)
+  loginAlertVariant.value = 'bg-green-500'
+  loginAlertMsg.value = 'Success! You are now logged in. '
+}
+</script>
+
+<style lang="scss" scoped></style>
